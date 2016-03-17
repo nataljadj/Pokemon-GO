93273c6db9221f52575ef11124ad8bd27d465e66
nataljadj <natalja.dj@gmail.com>
2016-03-17 18:01:11 +0200

    Load data from api

--- js/pokemonList/pokemonListController.js
+++ js/pokemonList/pokemonListController.js
@@ -8,14 +8,35 @@
 
     /**
      * @ngdoc controller
-     * @module tsmFunctionalities
-     * @name AssignmentController
-     * @description Contains logic for assignment functionality
+     * @module pokemonApp
+     * @name pokemonListController
+     * @description Contains logic for pokemon list
      *
      */
     angular
         .module('pokemonApp')
-        .controller('pokemonListController', function($scope){
+        .controller('pokemonListController', function ($scope, $http) {
 
+            function _loadDataFromApi() {
+                $http({
+                    method: 'GET',
+                    url: 'http://pokeapi.co/api/v1/pokemon/?limit=9'
+                }).then(function (data) {
+                    $scope.model = data.data;
+                });
+            }
+
+            $scope.getAbilities = function(objects) {
+                return _.pluck(objects.abilities, 'name');
+            };
+
+            $scope.loadMore = function(url){
+                $http.get('http://pokeapi.co/' + url).success(function(data){
+                    $scope.meta = data.meta;
+                    $scope.model.objects = $scope.model.objects.concat(data.objects);
+                });
+            };
+
+            _loadDataFromApi();
         });
 })(angular);
--- js/pokemonList/pokemonListDirective.js
+++ js/pokemonList/pokemonListDirective.js
@@ -3,31 +3,37 @@
  * Date of creation: 16.03.2016
  */
 
- (function (angular) {
- 'use strict';
+(function (angular) {
+    'use strict';
 
- /**
- *
- * *HTML*
- * ```html
- *  <pokemon-list></pokemon-list>
- * ```
- *
- */
-angular.module('pokemonApp').directive('pokemonList', [
-    function () {
-        return {
-            restrict: 'E',
-            scope: {},
-            controller: 'pokemonListController',
-            template: [
-                '   <div class="cgm-row">',
-                '       <div>',
-                '           <div>',
-                '           </div>',
-                '       </div>',
-                '   </div>'
-            ].join(' ')
-        };
-    }]);
+    /**
+     *
+     * *HTML*
+     * ```html
+     *  <pokemon-list></pokemon-list>
+     *
+     */
+    angular.module('pokemonApp').directive('pokemonList', [
+        function () {
+            return {
+                restrict: 'E',
+                scope: {},
+                controller: 'pokemonListController',
+                template: [
+                    '<div class="row">',
+                        '<div class="col-sm-5 col-md-4" ng-repeat="pokemon in model.objects">',
+                            '<div class="thumbnail">',
+                                '<img src="http://pokeapi.co/media/img/{{pokemon.pkdx_id}}.png " alt="..." class="pokemonPic">',
+                                '<div class="caption">',
+                                    '<h3>{{pokemon.name}}</h3>',
+                                    '<h4><span class="label label-warning">{{getAbilities(pokemon)[0]}}</span></h4>',
+                                '</div>',
+                            '</div>',
+                        '</div>',
+                        '<div class=""col-sm-6 col-md-6"><button type="button" class="btn btn-info btn-lg btn-block" ng-click="loadMore(model.meta.next)">Load more</button></div>',
+                    '</div>'
+                ].join(' ')
+            };
+        }]);
 }(angular));
+
